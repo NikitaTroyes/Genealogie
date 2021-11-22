fe034962e8c569f2a2821928cd8019f142ede3e3
NT <89306099+NikitaTroyes@users.noreply.github.com>
2021-11-22 11:39:58 +0100

    Add files via upload

---
+++ Genealogy.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genealogie
+{
+    public abstract class Person
+    {
+        protected string firstName;
+        protected string lastName;
+        protected int birthDate;
+        protected int deathDate;
+        protected string status; 
+
+        public Person (string firstName, string lastName, int birthDate, int deathDate, string status)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.birthDate = birthDate;
+            this.deathDate = deathDate;
+            this.status = status; 
+        }
+
+        public Person(string firstName, string lastName, int birthDate, string status)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.birthDate = birthDate;
+            this.status = status;
+        }
+
+        public Person(string firstName, string lastName, int birthDate, int deathDate)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.birthDate = birthDate;
+            this.deathDate = deathDate;
+        }
+
+        public Person(string firstName, string lastName, int birthDate)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.birthDate = birthDate;
+        }
+
+        public abstract void Add(Person person);
+
+        public abstract string GetData();
+
+    }
+
+    public class Genitor : Person
+    {
+        List<Person> listPerson = new List<Person>();
+
+        public Genitor(string firstName, string lastName, int birthDate, int deathDate) : base(firstName, lastName, birthDate, deathDate) { }
+        public Genitor(string firstName, string lastName, int birthDate) : base(firstName, lastName, birthDate) { }
+
+        public override void Add(Person person)
+        {
+            listPerson.Add(person); 
+        }
+
+        public override string GetData()
+        {
+            StringBuilder sbPerson = new StringBuilder();
+
+            foreach (Person person in listPerson)
+
+            {
+
+                sbPerson.Append(person.GetData() + "\n");
+
+            }
+
+            return sbPerson.ToString();
+        }
+    }
+
+    public class Member : Person
+    {
+        public Member(string firstName, string lastName, int birthDate, int deathDate, string status) : base(firstName, lastName, birthDate, deathDate, status) { }
+        public Member(string firstName, string lastName, int birthDate, string status) : base(firstName, lastName, birthDate, status) { }
+        public override void Add(Person person)
+        {
+        }
+
+        public override string GetData()
+        {
+            return "FirstName: " + firstName + "\nLastName: " + lastName + "\nBirthDate: " + birthDate.ToString() + "\nDeathDate: " + deathDate.ToString() + "\nStatus: " + status + "\n";  
+        }
+
+
+    }
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genealogie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person personA = new Member("Léon", "Blum", 1872, 1950, "oncle");
+            Person personB = new Member("Jules", "Ferry", 1832, 1896, "cousin" );
+            Person personC = new Member("Marie", "Curie", 1868, 1934, "tante");
+
+            Genitor newFamily = new Genitor("Simone", "Veil", 1927);
+            newFamily.Add(personA);
+            newFamily.Add(personB);
+            newFamily.Add(personC);
+
+            Console.WriteLine(newFamily.GetData());
+            Console.ReadLine(); 
+        }
+    }
+}
